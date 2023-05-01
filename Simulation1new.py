caf9d694d142f9ebf943ceb5ae3784933adffb48
Florent <59225340+FlorentGerbaud@users.noreply.github.com>
2023-05-01 19:08:52 +0200

    Modèle mathématique entièrement refait plus le code aussi

---
+++ Simulation1new.py
@@ -0,0 +1,59 @@
+###########################################                                        
+# _____________ BIBLIOTHEQUES ___________ #
+###########################################
+import numpy as np
+import time as time
+import matplotlib.pyplot as plt
+
+######### Méthode qui défini le nombre d'entrainement par semaine de l'athlète
+def N(E_t):
+    return E_t
+
+#méthode qui défini la qualité d'entrainement de l'athlète
+def E(E_t,N_t,alpha,beta):
+    E_tSuiv=E_t+N_t*(alpha-beta)
+    if(E_tSuiv>1):
+        return 1
+    if(E_tSuiv<-1):
+        return -1
+    else :
+        return E_tSuiv
+
+#méthode qui défini le niveau de confiance de l'athlète
+def C(C_t,E_t):
+    C_tSuiv=C_t+max(-C_t,E_t)
+    if(C_tSuiv>1):
+        return 1
+    else :
+        return C_tSuiv
+
+############################# CI #####################
+N0=3/30
+E0=0.5
+C0=0.3
+############################## Param #####################
+alpha=0.3
+beta=0.5
+########################## Valeur fonction ###############
+nbSemaine=52
+
+valN=np.zeros(nbSemaine)
+valE=np.zeros(nbSemaine)
+valC=np.zeros(nbSemaine)
+valE[0]=E0
+valN[0]=N0
+valC[0]=C0
+
+for dureePlan in range(1,nbSemaine):
+    valN[dureePlan]=N0
+    valE[dureePlan]=E(valE[dureePlan-1],valN[dureePlan-1],alpha,beta)
+    valC[dureePlan]=C(valC[dureePlan-1],valE[dureePlan-1])
+    
+print(valE)
+print(valC)
+plt.plot(np.arange(0,nbSemaine,1),valE,label="qualité des entrainements",c="black")
+plt.plot(np.arange(0,nbSemaine,1),valC,label="niveau de confiance",c="blue",linestyle="dashed")
+plt.xlabel("Times in weeks")
+plt.legend()
+plt.grid()
+plt.show()
